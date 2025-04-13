4d617047e70aae2b0179fc880fd3a6f9e7fd1b87
Hadi Nuno Handrison <hadinuno@gmail.com>
2025-04-14 03:55:04 +1000

    Changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
 // Replace 'your-username' and 'your-repo-name' with actual GitHub values
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base: '/nunohs-portfolio/', 
 });
 
